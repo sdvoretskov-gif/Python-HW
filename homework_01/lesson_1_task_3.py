26780295d129a2ef0e42547dbe7813d45037436a
sdvoretskov-gif <sdvoretskov@inbox.ru>
2026-05-28 21:54:17 +0400

    first_task

---
+++ homework_01/lesson_1_task_3.py
@@ -0,0 +1,3 @@
+first_name = input('Введите Ваше имя - ')
+last_name =input('Введите Вашу фамилию -')
+print('Вас зовут: ', last_name, first_name)
---
+++ homework_01/lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def function(num):
+    print(num, end='')     # 88005553535 = 11
+
+function(8)
+function(8)
+function(0)
+function(0)
+function(5)
+function(5)
+function(5)
+function(3)
+function(5)
+function(3)
+function(5)
+
