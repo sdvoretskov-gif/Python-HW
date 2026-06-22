72771f2d4655a47e016fa957de03148b6d8a7b3d
DNS <sdvoretskov@inbox.ru>
2026-06-22 10:57:50 +0400

    new2

---
+++ 04_lesson/calculator.py
@@ -0,0 +1,36 @@
+class Calculator:
+
+    @staticmethod
+    def sum(a, b):
+        result = a + b
+        return result
+
+    @staticmethod
+    def sub(a, b):
+        return a - b
+
+    @staticmethod
+    def mul(a, b):
+        return a * b
+
+    @staticmethod
+    def div(a, b):
+        if (b == 0):
+            raise ArithmeticError("На ноль делить нельзя")
+        return a / b
+
+    @staticmethod
+    def pow(a, b=2):
+        return a ** b
+
+    def avg(self, nums):
+        if (len(nums) == 0):
+            return 0
+
+        s = 0
+        for num in nums:
+            s = s + num
+
+        l = len(nums)
+        return self.div(s,l)
+
